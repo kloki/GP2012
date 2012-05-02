b3b571148ba496ecde1041d7ea917590caeb5644
Koen Klinkers <k.klinkers@gmail.com>
2012-05-02 13:51:55 +0200

    ddd

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
-function love.load()
-   love.graphics.setBackgroundColor(255,255,255)
+require('generateworld')
 
-   tilewidth=250
-   tileheight=200
+function love.load()
    plane=love.graphics.newImage("sprites/planesmall.gif")
-   mud=love.graphics.newImage("sprites/mud.gif")
-   leaves=love.graphics.newImage("sprites/leaves.gif")
-   water=love.graphics.newImage("sprites/water.gif")
+ 
+   --setup world
+   loadtiles()
    world=createworld()
 
 
@@ -26,6 +24,7 @@ function love.draw()
 end
 
 function love.update(dt)
+
    if love.keyboard.isDown("up") then
       yplane = yplane - 100*dt
       headingplane=0
@@ -42,30 +41,4 @@ function love.update(dt)
  
 end
 
-function createworld()
-   world = {}
-   for i=1,20 do
-      world[i]={}
-      for n=1,25 do
-	 world[i][n]=math.random(1,3)
-      end
-   end
-return world
-end
 
-function drawworld(world)
-   for i=1,20 do
-      for n=1,25 do
-	 if world[i][n]== 1 then
-	    love.graphics.draw(mud,(n-1)*32,(i-1)*32)
-	 end
-	 if world[i][n]== 2 then
-	    love.graphics.draw(leaves,(n-1)*32,(i-1)*32)
-	 end
-	 if world[i][n]== 3 then
-	    love.graphics.draw(water,(n-1)*32,(i-1)*32)
-	 end
-
-      end
-   end   
-end
