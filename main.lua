fd713ea4da68f862ead28c899af5f94b1f0d7828
tobyhijzen <toby.hijzen@gmail.com>
2012-05-02 14:34:02 +0200

    seperate move function

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require('movementHandler')
+
 function love.load()
    love.graphics.setBackgroundColor(255,255,255)
 
@@ -26,20 +28,7 @@ function love.draw()
 end
 
 function love.update(dt)
-   if love.keyboard.isDown("up") then
-      yplane = yplane - 100*dt
-      headingplane=0
-   elseif love.keyboard.isDown("down") then
-      yplane = yplane + 100*dt
-      headingplane=math.rad(180)
-   elseif love.keyboard.isDown("right") then
-      xplane = xplane + 100*dt
-      headingplane=math.rad(90)
-   elseif love.keyboard.isDown("left") then
-      xplane = xplane - 100*dt
-      headingplane=math.rad(270)
-   end
- 
+   movementHandler(dt)
 end
 
 function createworld()
