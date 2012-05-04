477f3684d4e8e54582e72f5b5bcd7f39ec1ee695
kriepy <k.rieping@gmail.com>
2012-05-04 15:25:17 +0200

    k

--- SpriteAnima.lua
+++ SpriteAnima.lua
@@ -1,10 +1,15 @@
 require ('lAnA')
 function loadSprite()
-    sheet = love.graphics.newImage("sprites/LinkMC.gif")
-    linkfrontS = love.graphics.newQuad(24,12,20,30,720,2630)
+   sheet = love.graphics.newImage("sprites/LinkMC.gif")
+   linkfrontS = love.graphics.newQuad(24,12,20,30,720,2630)
+   linkleftS = love.graphics.newQuad(24,44,20,30,720,2630)
+   linkbackS = love.graphics.newQuad(24,76,20,30,720,2630)
+   --linkleftS = linksideS:flip(true,false)
+   linkrightS = love.graphics.newQuad(24,44,20,30,720,2630)
     
-    linkfrontRun = newAnimation(sheet,23.5,32,0.1,10,59.5,12)
-    linksideRun = newAnimation(sheet,27,32,0.1,10,52,44)
+   linkfrontRun = newAnimation(sheet,23.5,32,0.1,10,59.5,12)
+   linksideRun = newAnimation(sheet,27,32,0.1,10,52,44)
+   linkbackRun = newAnimation(sheet,23,32,0.1,10,59.5,12)
     
 
     
@@ -13,23 +18,34 @@ end
 function drawSprite()
    --love.graphics.drawq(sheet,linkfrontS,xsprite,ysprite)
    if move == 'none' then
-      love.graphics.drawq(sheet,linkfrontS,xsprite,ysprite)
+      if stop == 'up' then
+         love.graphics.drawq(sheet,linkbackS,xsprite,ysprite)
+      elseif stop == 'right' then
+         love.graphics.drawq(sheet,linkrightS,xsprite,ysprite)
+      elseif stop == 'left' then
+         love.graphics.drawq(sheet,linkleftS,xsprite,ysprite)
+      else
+         love.graphics.drawq(sheet,linkfrontS,xsprite,ysprite)
+      end
    elseif move == 'right' then
       linksideRun:draw(xsprite,ysprite)
+   elseif move == 'down' then
+      linkfrontRun:draw(xsprite,ysprite)
+   elseif move == 'up' then
+      linkbackRun:draw(xsprite,ysprite)
+   elseif move == 'left' then
+      linksideRun:draw(xsprite,ysprite)
    end   
-    --[[if move then
-        linksideRun:draw(xplane,yplane)
-    else
-        love.graphics.drawq(sheet,linkfrontS,xplane,yplane)
-    end]]
-    --love.graphics.drawq(sheet,sp, xplane, yplane)
-    --love.graphics.drawq(sheet,linkfrontM[3], 200, 200)
-    --love.graphics.drawq(sheet,linkfrontM[4], 100, 100)
-    --love.graphics.drawq(sheet,linkfrontM[5], 10, 10)
-    --love.graphics.drawq(sheet,linkfrontM[6], 100, 10)
 end
 
 function updateSprite(dt)
-    --linkfrontRun:update(dt)
-    linksideRun:update(dt)
+   if move == 'down' then
+      linkfrontRun:update(dt)
+   elseif move == 'right' then
+      linksideRun:update(dt)
+   elseif move == 'left' then
+      linksideRun:update(dt)
+   elseif move == 'up' then
+      linkbackRun:update(dt)
+   end
 end
